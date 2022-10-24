6bd6cbbe121be914315e338f4bfd2bd1236fe3f0
Ifycode <marychord@gmail.com>
2022-10-24 03:34:22 +0100

    Section 2: Generating random password & checks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,11 +31,21 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`
-      Includes letters: ${this.includeLetters}
-      Includes numbers: ${this.includeNumbers}
-      Includes symbols: ${this.includeSymbols}
-    `);
-    this.password = 'MY PASSWORD!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&&*()';
+
+    let validChars = '';
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i += 1) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+
+    this.password = generatePassword;
   }
 }
